d99bf34371a54122343bc2d65f10b3179ff59fe6
Takudzwa Allen <takudzwamushai@gmail.com>
2026-04-24 16:00:35 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -19,3 +19,25 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => res.json({ message: "HAMS API running" }));
 
 // module.exports = app;
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((result) => {
+    app.listen(process.env.PORT || 5000);
+    console.log("Server started");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// export default app;
