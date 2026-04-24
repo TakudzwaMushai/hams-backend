27e231b7e775e65d148042ae28d5ab9110c854a3
Takudzwa Allen <takudzwamushai@gmail.com>
2026-04-24 17:33:03 +0100

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  if (mongoose.connections[0].readyState) return;
+let isConnected = false;
 
-  await mongoose.connect(process.env.MONGO_URI);
+const connectDB = async () => {
+  if (isConnected) return;
+
+  const db = await mongoose.connect(process.env.MONGO_URI);
+
+  isConnected = db.connections[0].readyState === 1;
 };
 
 module.exports = connectDB;
