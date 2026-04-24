94e54b7d40e63a5f7e3b4bb53dbe3d99073f84eb
Takudzwa Allen <takudzwamushai@gmail.com>
2026-04-24 12:08:57 +0100

    project init and setup

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const { swaggerUi, swaggerDocument } = require('./config/swagger');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => res.json({ message: 'HAMS API running' }));
+
+module.exports = app;
